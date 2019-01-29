45e3c1fc6cf57a64f1911c51c1e6e585e672fc03
Bronley Plumb <bronley@gmail.com>
2019-01-28 22:07:21 -0500

    Fiddling with making the repository npm-installable directly from github

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { BRSLanguageServer } from './BRSLanguageServer';
 import { BRSProgram } from './BRSProgram';
+import { BRSContext } from './BRSContext';
+import { BRSFile } from './BRSFile';
+import util from './util';
+import { Watcher } from './Watcher';
 
 export {
+    BRSContext,
+    BRSFile,
     BRSLanguageServer,
-    BRSProgram
+    BRSProgram,
+    util,
+    Watcher
 };
