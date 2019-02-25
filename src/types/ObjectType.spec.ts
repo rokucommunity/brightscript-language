99291a11877e2b4bc4624f03ef45914052afe2dd
Bronley Plumb <bronley@gmail.com>
2019-02-25 15:39:41 -0500

    Working on the object type

--- src/types/ObjectType.spec.ts
+++ src/types/ObjectType.spec.ts
@@ -1,10 +1,27 @@
 import { expect } from 'chai';
 import { DynamicType } from './DynamicType';
 import { ObjectType } from './ObjectType';
+import { StringType } from './StringType';
 
 describe('ObjectType', () => {
     it('is equivalent to other object types', () => {
         expect(new ObjectType().isAssignableTo(new ObjectType())).to.be.true;
         expect(new ObjectType().isAssignableTo(new DynamicType())).to.be.true;
+    });
+
+    describe('custom objects', () => {
+        it.only('matches objects with same properties', () => {
+            var personType = new ObjectType();
+            personType.addProperty('name', new StringType());
+
+            var dogType = new ObjectType();
+            dogType.addProperty('name', new StringType());
+            dogType.addProperty('breed', new StringType());
+
+            //person and dog both have "name", so dog can be used as person
+            expect(dogType.isAssignableTo(personType)).to.be.true;
+            //person does not have breed, so it cannot be used as dog
+            expect(personType.isAssignableTo(dogType)).to.be.false;
+        });
     });
 });
--- src/types/ObjectType.ts
+++ src/types/ObjectType.ts
@@ -2,17 +2,64 @@ import { BrsType } from './BrsType';
 import { DynamicType } from './DynamicType';
 
 export class ObjectType implements BrsType {
+    /**
+     * The list of properties for this object
+     */
+    public properties = [] as { name: string; type: BrsType }[];
+
+    /**
+     * Add a property to this object
+     * @param name 
+     * @param type 
+     */
+    public addProperty(name: string, type: BrsType) {
+        this.properties.push({
+            name: name,
+            type: type
+        })
+    }
+
+    /**
+     * Get the property with the specified name, or undefined if not found
+     * @param name 
+     */
+    public getProperty(name: string) {
+        for (let prop of this.properties) {
+            if (prop.name === name) {
+                return prop;
+            }
+        }
+    }
+
     public isAssignableTo(targetType: BrsType) {
-        return (
-            targetType instanceof ObjectType ||
-            targetType instanceof DynamicType
-        );
+        if (targetType instanceof DynamicType) {
+            return true;
+        } else if (targetType instanceof ObjectType) {
+            //the target must have all properties that this object has
+            for (let prop of this.properties) {
+                let targetProp = targetType.getProperty(prop.name);
+
+                //the target does not have this property
+                if (!targetProp) {
+                    return false;
+                }
+
+                //if the types are not same-like
+                if (targetProp.type.isAssignableTo(prop.type) === false) {
+                    return false;
+                }
+            }
+            //there were no issues with the type comparison, the objects are same-like
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public isConvertibleTo(targetType: BrsType) {
         return this.isAssignableTo(targetType);
     }
-    
+
     public toString() {
         return 'object';
     }
